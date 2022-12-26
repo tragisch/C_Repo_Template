8357d6cdf74790136943bc8b5501ce2787dc4b9c
tragisch <uwe@roettgermann.de>
2022-12-26 11:22:09 +0100

    fix size of array 9 -> 10

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include "include/armstrong_numbers.h"
 
 int main() {
-  int candidates[9] = {0, 9, 1, 4, 153, 188, 370, 371, 402, 407};
+  int candidates[10] = {0, 9, 1, 4, 153, 188, 370, 371, 402, 407};
 
   for (size_t i = 0; i < 9; i++) {
     bool armstrong = is_armstrong_number(candidates[i]);
     if (armstrong == false) {
-      printf("Number %zu is an amstrong number\n", i);
+      printf("Number %d is an amstrong number\n", candidates[i]);
     } else {
-      printf("Number %zu is NOT an amstrong number\n", i);
+      printf("Number %d is NOT an amstrong number\n", candidates[i]);
     }
   }
 
