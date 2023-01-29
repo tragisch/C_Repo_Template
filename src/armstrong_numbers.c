5a0a5ca006589a88bf12ad1d40840cecc2956e00
tragisch <uwe@roettgermann.de>
2023-01-29 12:25:17 +0100

    correct armstrong numbers

--- src/armstrong_numbers.c
+++ src/armstrong_numbers.c
@@ -1,19 +1,35 @@
 #include "armstrong_numbers.h"
 
+#include <stdio.h>
+
 bool is_armstrong_number(int candidate) {
   int r, sum = 0, temp;
+  int ndigits = n_digits(candidate);
 
-  temp = candidate;
-
-  while (candidate > 0) {
-    r = candidate % 10;
-    sum = sum + (r * r * r);
-    candidate = candidate / 10;
-  }
-
-  if (temp == sum) {
+  if (ndigits == 1) {
+    // number with a single digits are armstrong numbers
     return true;
+  } else if (ndigits == 2) {
+    // number with two single digits are not armstrong numbers
+    return false;
+  } else {  // number has more then two digits we have to check:
+    temp = candidate;
+
+    while (candidate > 0) {
+      r = candidate % 10;
+      sum = sum + (int)pow(r, ndigits);
+      candidate = candidate / 10;
+    }
+
+    if (temp == sum) {
+      return true;
+    }
   }
 
   return false;
+}
+
+int n_digits(int number) {
+  int x = log10(number) + 1;
+  return x;
 }
--- src/include/armstrong_numbers.h
+++ src/include/armstrong_numbers.h
@@ -1,9 +1,11 @@
 #ifndef ARMSTRONG_NUMBERS_H
 #define ARMSTRONG_NUMBERS_H
 
+#include <math.h>
 #include <stdbool.h>
 #include <string.h>
 
+int n_digits(int number);
 bool is_armstrong_number(int candidate);
 
 #endif
